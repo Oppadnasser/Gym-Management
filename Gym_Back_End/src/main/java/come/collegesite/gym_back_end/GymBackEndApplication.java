b5fa3acf6e5e220d0b80020483b7c1ef62b022aa
Oppadnasser <oppadnasser@gmail.com>
2025-10-15 10:52:03 +0300

    inital commit create back-end files

---
+++ Gym_Back_End/src/main/java/come/collegesite/gym_back_end/GymBackEndApplication.java
@@ -0,0 +1,13 @@
+package come.collegesite.gym_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymBackEndApplication.class, args);
+    }
+
+}
---
+++ Gym_Back_End/src/test/java/come/collegesite/gym_back_end/GymBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package come.collegesite.gym_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
